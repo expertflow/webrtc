2ee2325dd4f12161e11481e4fc7e7b43b7916fc1
farhanmaqbool99 <farhan.maqbool@expertflow.com>
2023-06-08 19:18:27 +0500

    update

--- script.js
+++ script.js
@@ -116,34 +116,15 @@ function startWebRTC(isOfferer) {
   }
 
   // When a remote stream arrives display it in the #remoteVideo element
-  // pc.ontrack = event => {
-  //   const stream = event.streams[0];
-  //   if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
-  //     remoteVideo.srcObject = stream;
-  //   }
-  // };
-  const remoteVideo = document.querySelector('#remoteVideo');
-
-  pc.addEventListener('track', async (event) => {
-    const [stream] = event.streams[0];
-    remoteVideo.srcObject = stream;
-  });
-
-
-
-
-
+  pc.ontrack = event => {
+    const stream = event.streams[0];
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+      remoteVideo.srcObject = stream;
+    }
+  };
 
   navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
     // Display your local video in #localVideo element
-    // const localStream = await getUserMedia({vide: true, audio: true});
-    // const peerConnection = new RTCPeerConnection(configuration);
-    // localStream.getTracks().forEach(track => {
-    //   peerConnection.addTrack(track, localStream);
-    // });
-
-
-
     streamObj.localStream = stream;
     localVideo.srcObject = streamObj.localStream;
     // Add your stream to be sent to the conneting peer
